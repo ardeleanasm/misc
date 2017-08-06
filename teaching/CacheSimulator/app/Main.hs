bcee640236ac3e9235fa9d04b9951f678aec1fee
ardeleanasm <ardeleanasm@gmail.com>
2017-08-06 21:51:58 +0300

    removed

--- teaching/CacheSimulator/app/Main.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-
-import CacheSimulator
-
-main :: IO ()
-main = someFunc
--- teaching/CacheSimulator/src/CacheSimulator.hs
+++
@@ -1,6 +0,0 @@
-module CacheSimulator
-    ( someFunc
-    ) where
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
