62c44311e5d0f68fec34859eea5ba57e5bc0e274
ardeleanasm <ardeleanasm@gmail.com>
2018-06-13 20:08:25 +0300

    new app

---
+++ haskell/hasher/app/Main.hs
@@ -0,0 +1,43 @@
+module Main where
+
+import Lib
+
+import System.Exit
+import System.Environment(getArgs,getProgName)
+import Text.Read
+
+
+
+
+printVersion::String->String->IO()
+printVersion appName appVersion=putStrLn $ appName++ " Version: "++appVersion
+
+
+help::String->IO()
+help appName=do
+  putStrLn (appName++" <parameter> [<value>]")
+  putStrLn "parameters:"
+  putStrLn "\t\t-h/-help:\t help"
+  putStrLn "\t\t-v:\t\t version"
+  putStrLn "\t\t-f file:\thash file"
+  putStrLn "\t\t-c hash:\thash function"
+  putStrLn "\t\t-s string:\tplaintext"
+
+parseArguments::IO()
+parseArguments=do
+  args<- getArgs
+  appName<-getProgName
+  case args of
+    ["-h"]->help appName>>exitWith ExitSuccess
+    ["-help"]->help appName>>exitWith ExitSuccess
+    ["-v"]->printVersion appName "0.1.0.0">>exitWith ExitSuccess
+    ["-f",value]->putStrLn value
+    ["-c",value]->putStrLn value
+    ["-s",value]->putStrLn value
+    []->help appName>>exitWith (ExitFailure 1)
+
+
+  
+main :: IO ()
+main = do
+  parseArguments
