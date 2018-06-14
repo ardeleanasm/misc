55b8eacf9158d0fce7c4036f66d2eb1e1c299b27
ardeleanasm <ardeleanasm@gmail.com>
2018-06-14 23:17:45 +0300

    highorder function

--- ocaml/monte-carlo-integration/lib/MonteCarlo_type.mli
+++ ocaml/monte-carlo-integration/lib/MonteCarlo_type.mli
@@ -1,7 +1,7 @@
 module type MonteCarlo_type=sig
   val initializePRNG:unit->unit
   val getRandomNumber:unit->float
-  val approximate:float->float->int->int->float->float  
+  val approximate:(float->float)->float->float->int->int->float->float  
 end
 
 
--- ocaml/monte-carlo-integration/src/Main.ml
+++ ocaml/monte-carlo-integration/src/Main.ml
@@ -1,15 +1,11 @@
 open MonteCarlo
 
 
-(*
-F=(b-a)*1/N* sum{0}{N-1}f(X)
-
-*)
 
 let func (x:float):float=2.0*.x+.3.0
 
 (* let pi = 4.0 *. atan 1.0;; *)
-
+(* let func (x:float)=sin x *)
 let () = 
   MonteCarlo.initializePRNG();
-  print_float (MonteCarlo.approximate 0.0 5.0 100000 0 0.0)
+  print_float (MonteCarlo.approximate func 0.0 5.0 100000 0 0.0)
